5bb299a8ab1bcda2f9d08c8ecd9f5e068f283f6d
Songmin Li <lisongmin@protonmail.com>
2024-05-18 18:07:54 +0800

    chore: remove unused init function

--- cmd/pg-helper/cmd_serve.go
+++ cmd/pg-helper/cmd_serve.go
@@ -34,27 +34,3 @@ func (s *ServeCmd) Run(ctx *Context) error {
 	server.Run()
 	return nil
 }
-
-/*
-func init() {
-	rootCmd.AddCommand(serveCmd)
-
-	viper.SetDefault("web::use_h2c", true)
-	viper.SetDefault("web::trusted_proxies", []string{})
-
-	viper.SetDefault("db::host", "127.0.0.1")
-	viper.SetDefault("db::port", 5432)
-	viper.SetDefault("db::user", "postgres")
-	viper.SetDefault("db::db_name", "postgres")
-	viper.SetDefault("db::reserve_db_names", []string{"postgres", "template0", "template1"})
-	viper.SetDefault("db::max_conns", 4)
-	viper.SetDefault("db::migrations_path", "file:///usr/share/pg-helper/migrations")
-	viper.SetDefault("db::backup_root_path", "/var/lib/pg-helper/backups")
-	viper.SetDefault("remote_helper::url_template", "http://pg-%d")
-
-	viper.BindEnv("db::password_file", "PG_HELPER_DB_PASSWORD_FILE")
-	viper.BindEnv("db::password", "PG_HELPER_DB_PASSWORD")
-	viper.BindEnv("db::host", "PG_HELPER_DB_HOST")
-	viper.BindEnv("db::current_db_version", "PG_MAJOR")
-}
-*/
